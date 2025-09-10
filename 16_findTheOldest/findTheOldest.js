5669b08431c809df20363814be8ed764ccf0123a
Alawi <dukstv48@gmail.com>
2025-09-11 00:00:48 +0800

    Alawi test 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  const currentYear = new Date().getFullYear();
 
+  return people.reduce((oldest, person) => {
+    const personDeath = person.yearOfDeath || currentYear;
+    const oldestDeath = oldest.yearOfDeath || currentYear;
+
+    const personAge = personDeath - person.yearOfBirth;
+    const oldestAge = oldestDeath - oldest.yearOfBirth;
+
+    return personAge > oldestAge ? person : oldest;
+  });
 };
 
 // Do not edit below this line
